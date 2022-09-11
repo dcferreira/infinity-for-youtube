6c0942f478bfd40dff594593098073a9703b17f4
Daniel Ferreira <daniel.ferreira.1@gmail.com>
2022-09-11 21:14:24 +0200

    remove tooltip (and fix remove progress bar)

--- source/watch-page.ts
+++ source/watch-page.ts
@@ -21,57 +21,119 @@ async function checkCurrentVideo(
 	return false;
 }
 
-async function removeProgressBar() {
-	const progressBar = document.querySelector('.ytp-progress-bar-container');
-	const channels = getChannels();
-	const removeNeeded = await checkCurrentVideo(channels);
-	if (removeNeeded && progressBar) {
-		progressBar.remove();
+class FutureElement {
+	// eslint-disable-next-line @typescript-eslint/ban-types
+	element: HTMLElement | null;
+	query: string;
+	baseElement: HTMLElement;
+
+	constructor(selector: string, base: HTMLElement = document.body) {
+		this.query = selector;
+		this.element = null;
+		this.baseElement = base;
+	}
+
+	exists(): boolean {
+		if (!this.element) {
+			this.element = this.baseElement.querySelector(this.query);
+		}
+
+		return this.element !== null;
+	}
+
+	async show() {
+		if (this.exists()) {
+			this.element!.style.setProperty('opacity', '1');
+		}
+	}
+
+	async hide() {
+		if (this.exists()) {
+			this.element!.style.setProperty('opacity', '0');
+		}
+	}
+
+	async monitorNode(callback: () => Promise<void>) {
+		// Wait for element to appear, and call the callback function when it comes
+		const observer = new MutationObserver((mutations) => {
+			for (const mutation of mutations) {
+				if (mutation.type === 'childList' && this.exists()) {
+					void callback();
+					observer.disconnect();
+				}
+			}
+		});
+
+		observer.observe(this.baseElement, {
+			childList: true,
+			subtree: true,
+		});
 	}
 }
+
+// async function showElement(element: HTMLElement | undefined) {
+// 	if (element) element.style.setProperty('opacity', '1');
+// }
+
+// async function hideElement(element: HTMLElement) {
+// 	element.style.setProperty('opacity', '0');
+// }
 
 async function removeTotalTime(duration: string) {
 	// Wait to get the channel information
 	const currChannel = await waitForElement('#meta-contents #channel-name a');
 
 	// Fetch the time and set up a function to change it
-	const time = document.querySelector('.ytp-time-duration')!;
-	async function setTime(node: HTMLElement) {
+	const player: HTMLElement = document.querySelector('#movie_player')!;
+	const time = new FutureElement('.ytp-time-duration', player);
+	const progressBar = new FutureElement('.ytp-progress-bar-container', player);
+	const tooltip = new FutureElement('div.ytp-tooltip.ytp-bottom', player);
+	async function updatePlayerElements() {
 		const channels = getChannels();
 		const removeNeeded = await checkCurrentVideo(channels);
-		if (removeNeeded && node.textContent !== duration) {
+
+		const timeElement = time.element!;
+		if (removeNeeded && timeElement.textContent !== duration) {
 			// Change text, but store the old value in the element
-			if (time.textContent) time.setAttribute('old-time', time.textContent);
-			else time.setAttribute('old-time', '0');
-			time.textContent = duration;
+			if (timeElement.textContent)
+				timeElement.setAttribute('old-time', timeElement.textContent);
+			else timeElement.setAttribute('old-time', '0');
+			timeElement.textContent = duration;
+
+			void progressBar.hide();
+			void tooltip.hide();
 		} else if (
 			!removeNeeded &&
-			node.textContent === duration &&
-			time.hasAttribute('old-time')
+			timeElement.textContent === duration &&
+			timeElement.hasAttribute('old-time')
 		) {
 			// If the channel changes after the video time was reset,
-			// we can restore the time
-			time.textContent = time.getAttribute('old-time')!;
+			// we can restore the elements
+			timeElement.textContent = timeElement.getAttribute('old-time')!;
+
+			void progressBar.show();
+			void tooltip.show();
 		}
 	}
 
-	if (time) {
-		const emptyElement = document.createElement('div');
-		void setTime(emptyElement);
+	void tooltip.monitorNode(updatePlayerElements);
+
+	if (time.exists()) {
+		void updatePlayerElements();
 
 		// Monitor changes to the time
 		const timeObserver = new MutationObserver((mutations) => {
 			// Monitor the total video time for changes (ad starts, or a new video without page refresh)
 			for (const mutation of mutations) {
 				if (mutation.type === 'childList') {
-					for (const node of mutation.addedNodes) {
-						void setTime(node as HTMLElement);
+					for (const _node of mutation.addedNodes) {
+						void updatePlayerElements();
 					}
 				}
 			}
 		});
 
-		timeObserver.observe(time, {
+		timeObserver.observe(time.element!, {
 			childList: true,
 		});
 
@@ -80,8 +142,8 @@ async function removeTotalTime(duration: string) {
 			// Monitor the channel name, it's slower to refresh than the video time
 			for (const mutation of mutations) {
 				if (mutation.type === 'childList') {
-					for (const node of mutation.addedNodes) {
-						void setTime(time as HTMLElement);
+					for (const _node of mutation.addedNodes) {
+						void updatePlayerElements();
 					}
 				}
 			}
@@ -94,5 +156,5 @@ async function removeTotalTime(duration: string) {
 	}
 }
 
-void removeProgressBar();
+// void removeProgressBar();
 void removeTotalTime('∞');
